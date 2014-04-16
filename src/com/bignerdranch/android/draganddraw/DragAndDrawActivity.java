9f79767526a953f87c587e3811757ab6cc1d48f4
Gary Danovich <danovichg05@mycu.concord.edu>
2014-04-16 16:33:09 -0400

    Chapter 32 completed
    Final commit, after messing up the commit name (saying chapter 33).

--- src/com/bignerdranch/android/draganddraw/DragAndDrawActivity.java
+++ src/com/bignerdranch/android/draganddraw/DragAndDrawActivity.java
@@ -1,64 +1,10 @@
 package com.bignerdranch.android.draganddraw;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
-public class DragAndDrawActivity extends ActionBarActivity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_drag_and_draw);
-
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if present.
-		getMenuInflater().inflate(R.menu.drag_and_draw, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_drag_and_draw,
-					container, false);
-			return rootView;
-		}
-	}
-
+public class DragAndDrawActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new DragAndDrawFragment();
+    }
 }
