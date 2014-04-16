12327aa323bd91b864ff57c83d286653d6d6cda3
Gary Danovich <danovichg05@mycu.concord.edu>
2014-04-16 16:17:31 -0400

    Chapter 33 (drawing boxes) done

--- src/com/bignerdranch/android/draganddraw/DragAndDrawActivity.java
+++ src/com/bignerdranch/android/draganddraw/DragAndDrawActivity.java
@@ -11,54 +11,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-public class DragAndDrawActivity extends ActionBarActivity {
+public class DragAndDrawActivity extends SingleFragmentActivity 
+{
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_drag_and_draw);
-
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+	public Fragment createFragment()
+	{
+		return new DragAndDrawFragment();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.drag_and_draw, menu);
-		return true;
-	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_drag_and_draw,
-					container, false);
-			return rootView;
-		}
-	}
 
 }
